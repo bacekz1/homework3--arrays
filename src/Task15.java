4d20e1991bea08e47a5e65a39d20d5e70ad82738
Bacekz1 <vasildinchev@gmail.com>
2022-06-30 00:53:20 +0300

    Fix "bigger" case in 15 task.

--- src/Task15.java
+++ src/Task15.java
@@ -1,23 +1,31 @@
 public class Task15 {
     public static void main(String[] args) {
 
+        double[] arr = {-50.4, 11.15, 99.65165, 15.8, 22.6, -185.6};
 
-        double[] arr = {-50.4, 11.15, 99.65165, 15.8, 22.6};
-        double big = Double.MIN_VALUE;
-        double bigger = Double.MIN_VALUE;
-        double biggest = Double.MIN_VALUE;
+        double big = 0;
+        double bigger = 0;
+        double biggest = 0;
 
         for (int index = 0; index < arr.length; index++) {
-            double currentNum =  arr[index];
-             if (Math.abs(currentNum) > Math.abs(biggest)){
-                 bigger = biggest;
-                 biggest = currentNum;
-             }
-             if ( Math.abs(currentNum) < Math.abs(bigger) && Math.abs(currentNum) > Math.abs(big)){
-                 big = currentNum;
-             }
+            double currentNum = arr[index];
+
+            if (Math.abs(currentNum) > Math.abs(biggest)) {
+                bigger = biggest;
+                biggest = arr[index];
+            }
+
+            if (Math.abs(currentNum) < Math.abs(biggest) && Math.abs(currentNum) > Math.abs(bigger)) {
+                big = bigger;
+                bigger = arr[index];
+            }
+
+            if (Math.abs(currentNum) < Math.abs(bigger) && Math.abs(currentNum) > Math.abs(big)) {
+                big = currentNum;
+            }
 
         }
+
         System.out.printf("%f; %f; %f", big, bigger, biggest);
     }
 }
