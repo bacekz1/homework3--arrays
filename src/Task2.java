c04da127b44ea60a6129fb07dcbe64c6f81761dc
Bace_kz <vasildinchev@gmail.com>
2022-07-03 22:31:36 +0300

    fix if arr % 2 !=0 in task 2.

--- src/Task2.java
+++ src/Task2.java
@@ -1,13 +1,17 @@
 public class Task2 {
     public static void main(String[] args) {
-        int[] arr = {10, 3, 5, 8, 6, -3, 7, 11};
+        int[] arr = {10, 3, 5, 8, 6, -3, 7, 11, 1};
         int[] result = new int[arr.length];
-        int count = arr.length;
+        int arrLength = arr.length;
+        if (arr.length % 2 != 0) {
+            int temp = arr.length / 2;
+            result[temp] = arr[temp];
+        }
 
         for (int index = 0; index < arr.length / 2; index++) {
-            count--;
+            arrLength--;
             result[index] = arr[index];
-            result[count] = arr[index];
+            result[arrLength] = arr[index];
         }
 
         for (int index = 0; index < result.length; index++) {
